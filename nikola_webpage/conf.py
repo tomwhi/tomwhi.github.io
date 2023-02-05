495210c687f48e27650891b500b8ec8ef4b971aa
Thomas Whitington <thomaswhitington@gmail.com>
2023-02-05 17:09:53 +0100

    fixing link

--- nikola_webpage/conf.py
+++ nikola_webpage/conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Tom Whitington"
 BLOG_TITLE = "Tom's Data Science Blog"
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://tomwhi.github.com/"
+SITE_URL = "https://tomwhi.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
